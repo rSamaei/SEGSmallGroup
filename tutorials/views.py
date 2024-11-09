71ee02c73bb66ad5f9f837dc31bc3121e63fcd7b
Mohammed Shafayat Hossain <tayeen.mh@gmail.com>
2024-11-09 18:38:21 +0000

    Request show up on dashboard

--- tutorials/views.py
+++ tutorials/views.py
@@ -10,14 +10,23 @@ from django.views.generic.edit import FormView, UpdateView
 from django.urls import reverse
 from tutorials.forms import LogInForm, PasswordForm, UserForm, SignUpForm
 from tutorials.helpers import login_prohibited
+from tutorials.models import RequestSession
 
 
 @login_required
 def dashboard(request):
     """Display the current user's dashboard."""
-
     current_user = request.user
-    return render(request, 'dashboard.html', {'user': current_user})
+    list = []
+    for e in RequestSession.objects.all():
+        if e.student == current_user:
+            tempDict = {'subject':e.subject.name,'prof':e.proficiency,'freq':e.frequency,'date':e.date_requested.strftime('%d/%m/%Y')}
+            list.append(tempDict)
+    noRequestMessage = ''
+    if len(list) == 0:
+        noRequestMessage = 'No requests were found'
+
+    return render(request, 'dashboard.html', {'user': current_user, 'requests':list, 'message':noRequestMessage})
 
 
 @login_prohibited
