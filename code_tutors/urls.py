658c8262ce9c59a7c18794d0ee59eb28de2ee4f0
Mushtaq Ahmad <159441920+Mushtaq-Ahmad1@users.noreply.github.com>
2024-11-14 13:51:08 +0000

    Update urls.py

--- code_tutors/urls.py
+++ code_tutors/urls.py
@@ -29,5 +29,6 @@ urlpatterns = [
     path('password/', views.PasswordView.as_view(), name='password'),
     path('profile/', views.ProfileUpdateView.as_view(), name='profile'),
     path('sign_up/', views.SignUpView.as_view(), name='sign_up'),
+    path('add_request/', views.AddRequestView.as_view(), name='add_request'),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
