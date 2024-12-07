c86eced96050a37f33c88fa10d043a53cd61da8b
rSamaei <k23005640@kcl.ac.uk>
2024-12-07 00:35:34 +0000

    hotfix calendar not working, now fixed

--- tutorials/views.py
+++ tutorials/views.py
@@ -780,15 +780,15 @@ def get_recurring_dates(session, year, month):
             (date(request_date.year + 1, 4, 15), date(request_date.year + 1, 7, 20))  # Summer
         ]
     elif 9 < request_date.month <= 12:
-        academic_year_start = date(request_date.year, 1, 4)
-        academic_year_end = date(request_date.year, 7, 20)
+        academic_year_start = date(request_date.year + 1, 1, 4)
+        academic_year_end = date(request_date.year + 1, 7 + 1, 20)
         term_dates = [
-            (date(request_date.year, 1, 4), date(request_date.year, 3, 31)),  # Spring
-            (date(request_date.year, 4, 15), date(request_date.year, 7, 20))  # Summer
+            (date(request_date.year + 1, 1, 4), date(request_date.year + 1, 3, 31)),  # Spring
+            (date(request_date.year, 4, 15 + 1), date(request_date.year + 1, 7, 20))  # Summer
         ]
     else:
         academic_year_start = date(request_date.year, 4, 15)
-        academic_year_end = date(request_date.year, 7, 20)
+        academic_year_end = date(request_date.year + 1, 7, 20)
         term_dates = [
             (date(request_date.year, 4, 15), date(request_date.year, 7, 20))  # Summer
         ]
@@ -801,7 +801,7 @@ def get_recurring_dates(session, year, month):
         in_term = any(term_start <= current <= term_end for term_start, term_end in term_dates)
         if in_term and pycalendar.day_name[current.weekday()] in session_days and current.month == month and current.year == year:
             dates.append(current.day + 1)
-            current += timedelta(days=interlude)
+            current += timedelta(days=interlude) 
         else:
             current += timedelta(days=1)
     
